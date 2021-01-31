8308eab79c7ba8806e8a3185df291bac83727ddd
Jonathan Gerardo <67252997+jgerardo214@users.noreply.github.com>
2021-01-31 14:04:22 -0800

    Add files via upload

---
+++ Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe1.0
+//
+//  Created by Jonathan Gerardo on 1/28/21.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
